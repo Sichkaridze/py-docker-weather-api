ed0e03afba76a4d1b15e92f0c7e9f8e22daba210
Dmytro <dima.sichkar2003@gmail.com>
2025-02-18 15:25:59 +0200

    Docker weather API for Paris

--- app/main.py
+++ app/main.py
@@ -1,7 +1,60 @@
+import os
+import requests
+from pprint import pprint
+from dotenv import  load_dotenv
+
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+URL = "http://api.weatherapi.com/v1/current.json"
+#URL for getting current weather, for other services check endpoints on
+#https://app.swaggerhub.com/apis-docs/WeatherAPI.com/WeatherAPI/1.0.2
+
+LOCATION = "Paris"
+#Pass US Zipcode, UK Postcode, Canada Postalcode,
+# IP address, Latitude/Longitude (decimal degree) or city name.
+# Visit https://www.weatherapi.com/docs/#intro-request to learn more.
+
+LANGUAGE = None
+# LANGUAGE = "uk" #//uncomment it if you want to use language localization
+# Returns 'condition:text' field in API in the desired language.
+# Visit https://www.weatherapi.com/docs/#intro-request to check 'lang-code'.
+
+PARAMS = {
+    "q": LOCATION,
+    "key": API_KEY
+}
+
+if LANGUAGE:
+    PARAMS["lang"] = LANGUAGE
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    response = requests.get(URL, params=PARAMS)
 
+    try:
+        assert response.status_code == 200
+    except AssertionError as _:
+        print("Bad request")
 
+    data = response.json()
+
+    current_weather = data.get("current")
+    location = data.get("location")
+
+    temp_c = current_weather.get("temp_c")
+    last_updated = current_weather.get("last_updated")
+    weather_condition = current_weather.get("condition").get("text")
+
+    city = location.get("name")
+    country = location.get("country")
+
+    print(f"Performing request to Weather API for city {city}...")
+    print(
+        f"{city}/{country} {last_updated} Weather: "
+        f"{temp_c} Celsius, {weather_condition}"
+    )
 if __name__ == "__main__":
     get_weather()
